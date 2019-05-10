d6b4da217cc15030b4b85a31acff83f31b4b96d8
gustavocorreia <gustavocorreiasantos@outlook.com>
2019-05-10 00:16:03 -0300

    Initial commit

---
+++ app/src/main/java/br/com/fiap/gcs/demoqrcode/LeitorQRCodeActivity.kt
@@ -0,0 +1,12 @@
+package br.com.fiap.gcs.demoqrcode
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class LeitorQRCodeActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_leitor_qrcode)
+    }
+}
